4ddacf440d2adab09dbb1c2dee05576863d5551e
Fadhil <fadhilahdwi.a@gmail.com>
2024-07-13 23:28:39 +0700

    feat: update meta data

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,29 @@ import MovieDetailPage from './pages/MovieDetailPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
 import Header from './components/Header';
+import { Helmet } from 'react-helmet';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-primary scroll-smooth">
-        <Header />
-        <main className="flex-grow p-4 pt-24">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movie/:id" element={<MovieDetailPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <>
+      <Helmet>
+        <meta name="description" content="This is a movie app built with React and Vite" />
+        <title>Movie App</title>
+      </Helmet>
+      <Router>
+        <div className="flex flex-col min-h-screen bg-primary scroll-smooth">
+          <Header />
+          <main className="flex-grow p-4 pt-24">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/movie/:id" element={<MovieDetailPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </>
   );
 };
 
